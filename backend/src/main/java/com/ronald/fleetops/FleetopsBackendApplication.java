23c1d31711ee0da466e4cb4c1235d3b9302882e3
Ronald00237 <tayloronaldkemta@gmail.com>
2026-07-23 01:08:56 +0100

    chore: initialize Spring Boot backend

---
+++ backend/src/main/java/com/ronald/fleetops/FleetopsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ronald.fleetops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FleetopsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FleetopsBackendApplication.class, args);
+	}
+
+}
